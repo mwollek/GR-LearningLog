5586b4a2567c666eec25bc65a4e0f47dfc434b55
mwollek <wollekmikolaj@gmail.com>
2020-05-31 12:26:13 +0200

    Added Topics displaying Updated base.html: added hrefs for Topics

--- LearningLogs/urls.py
+++ LearningLogs/urls.py
@@ -5,5 +5,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # Home page
+    url(r'^$', views.index, name='index'),
+
+    # Page with topics
+    url(r'^topics/$', views.topics, name='topics'),
+
 ]
--- LearningLogs/views.py
+++ LearningLogs/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
 
 
 def index(request):
+    """Displaying home page"""
     return render(request, 'LearningLogs/index.html')
+
+
+def topics(request):
+    """Displaying page with topics"""
+    topics = Topic.objects.order_by('posted_date')
+    context = {'topics': topics}
+    return render(request, 'LearningLogs/topics.html', context)
