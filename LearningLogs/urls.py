b7b340fe73af7200f1795894f1d5d7ad87a52382
mwollek <wollekmikolaj@gmail.com>
2020-06-05 21:53:09 +0200

    Entry adding and editing functionality added.

--- LearningLogs/urls.py
+++ LearningLogs/urls.py
@@ -6,11 +6,20 @@ from . import views
 
 urlpatterns = [
     # Home page
-    url(r'^$', views.index, name='index'),
+    url(r'^$', view=views.index, name='index'),
 
     # Page with topics
-    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/$', view=views.topics, name='topics'),
 
     # Page for single topic
-    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    url(r'^topics/(?P<topic_id>\d+)/$', view=views.topic, name='topic'),
+
+    # Page for adding topic
+    url(r'^new_topic/$', view=views.new_topic, name='new_topic'),
+
+    # Page for adding entry for topic
+    url(r'^new_entry/(?P<topic_id>\d+)/$', view=views.new_entry, name='new_entry'),
+
+    # Page for editing entries
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', view=views.edit_entry, name='edit_entry'),
 ]
--- LearningLogs/views.py
+++ LearningLogs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -21,3 +24,53 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-posted_date')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'LearningLogs/topic.html', context)
+
+
+def new_topic(request):
+    """Creates form for new topic, or import new topic to database"""
+    if request.method != 'POST':
+        # No data has been given so new form has to be created.
+        form = TopicForm()
+    else:
+        # Data has been given as POST request, it has to be processed
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('LearningLogs:topics'))
+    context = {'form': form}
+    return render(request, 'LearningLogs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Creates form for new entry connected with topic -> topic_id"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('LearningLogs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'LearningLogs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Editing form"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('LearningLogs:topic', args=[topic.id]))
+    context = {'topic': topic, 'entry': entry, 'form': form}
+    return render(request, 'LearningLogs/edit_entry.html', context)
+    return None
